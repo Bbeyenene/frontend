bb6052e4183d76f4e8f8a0b1fbe2cd9d11b477cb
Berhane Beyene <bbeyene@MacBook-Pro.local>
2022-03-02 02:12:26 -0600

    updated code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import {useEffect, useState} from 'react'
-import axios from 'axios'
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [data, setData] = useState({})
-  useEffect(()=>{
-    axios.get('/api').then(res=>{
-      console.log('data: ',res.data)
-      setData(res.data)
-    })
-  }, [])
+  const [data, setData] = useState({});
+  useEffect(() => {
+    axios.get("/api").then((res) => {
+      console.log("data: ", res.data);
+      setData(res.data);
+    });
+  }, []);
+  useEffect(() => {
+    axios.get("/products").then((res) => {
+      console.log("data: ", res.data);
+      setData(res.data);
+    });
+  }, []);
 
   return (
     <div className="App">
       <h1>Hello Berhane</h1>
       <p>an awsome work</p>
       <h2>{data.mesg}</h2>
-
     </div>
   );
 }
